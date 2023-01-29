78cddb3f639b69fa3a7ed869b37501609f15436a
Devison Santos <devison_santos@yahoo.com.br>
2023-01-29 12:07:54 -0300

    :truck: chore: Movendo arquivos para pastas com o mesmo nome dos modulos do tutorial

--- path_parameters/path_containing_paths.py
+++ path_parameters/path_containing_paths.py
@@ -5,7 +5,7 @@ app = FastAPI()
 @app.get('/file/{file_path:path}')
 async def read_file(file_path: str):
     """
-        Se o path receber um paths, tem que ser usado o path conversor {file_path:path} 
+        Se o path decorator receber um paths, tem que ser usado o path conversor {file_path:path} 
         e a chamada tem que ter // no inicio do paths  
     """
     with open(file_path, 'r') as file:
